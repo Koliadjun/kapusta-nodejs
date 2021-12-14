24fd3a50fc31183e45b7a604b221be812c9893b7
bogdankol <bogdank208@gmail.com>
2021-12-14 20:23:28 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,14 +1,12 @@
 
 const { registrationValidation } = require('./registrationValidation')
 const { loginValidation } = require('./loginValidation')
-const { existenceCheck } = require('./existenceCheck')
 const { authenticateUser } = require('./authenticateUser')
 const validationWrapper = require('./validationWrapper')
 
 module.exports = {
   registrationValidation,
   loginValidation,
-  existenceCheck,
   authenticateUser,
   validationWrapper,
 }

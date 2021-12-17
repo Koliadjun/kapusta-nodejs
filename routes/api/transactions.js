0e5b1194c7f5d94f94ac6ceef8cdfdaee43c0d8e
Misha Krasnonos <muzishutka7@gmail.com>
2021-12-17 21:31:53 +0200

    Update transactions.js

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { transactions } = require('../../controllers')
 const { errorHandler } = require('../../helpers')
 const { validationWrapper, authenticateUser } = require('../../middlewares')
-const schema = require('../../middlewares/schemaJoi')
+const schema = require('../../middlewares/')
 const router = express.Router()
 
 router.get('/', authenticateUser, errorHandler(transactions.getAll))
@@ -13,5 +13,7 @@ router.post(
   validationWrapper(schema),
   errorHandler(transactions.addIncome)
 )
+
+router.delete('/:id', authenticateUser, errorHandler(transactions.deleteById))
 
 module.exports = router

8ffbcc8fa27fb1252ddaaee27743234674846196
Misha Krasnonos <muzishutka7@gmail.com>
2021-12-18 21:12:56 +0200

    fix Schema

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { transactions } = require('../../controllers')
 const { errorHandler } = require('../../helpers')
 const { validationWrapper, authenticateUser } = require('../../middlewares')
-const schema = require('../../middlewares/')
+const schema = require('../../middlewares/schemaJoi')
 const router = express.Router()
 
 router.get('/', authenticateUser, errorHandler(transactions.getAll))

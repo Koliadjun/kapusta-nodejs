f012ced63377f44fa193808696a67ce0cd620213
Misha Krasnonos <muzishutka7@gmail.com>
2021-12-18 17:10:30 +0200

    up

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,12 +1,31 @@
 const express = require('express')
-const {registrationValidation, loginValidation, authenticateUser} = require('../../middlewares')
+const {
+  registrationValidation,
+  loginValidation,
+  authenticateUser,
+} = require('../../middlewares')
 const authRouter = express.Router()
-const {registrationController, loginController, logoutController, verifyUserController} = require('../../controllers')
-const {errorHandler} = require('../../helpers')
+const {
+  registrationController,
+  loginController,
+  logoutController,
+  verifyUserController,
+} = require('../../controllers')
+const { errorHandler } = require('../../helpers')
 
-authRouter.post('/registration', registrationValidation, errorHandler(registrationController))
+authRouter.post(
+  '/registration',
+  registrationValidation,
+  errorHandler(registrationController)
+)
 authRouter.post('/login', loginValidation, errorHandler(loginController))
-authRouter.get('/logout',  authenticateUser, logoutController)
+authRouter.get('/logout', authenticateUser, logoutController)
 authRouter.get('/verify/:verificationToken', errorHandler(verifyUserController))
+authRouter.patch(
+  '/:id/budget',
+  authenticateUser,
+  //   validation(schemaFavorite),
+  errorHandler()
+)
 
 module.exports = authRouter

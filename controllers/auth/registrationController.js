305ac0c7ee90a9b00d22f6d4731b7a0e6fbcb905
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-23 15:25:02 +0200

    disable verification email

--- controllers/auth/registrationController.js
+++ controllers/auth/registrationController.js
@@ -1,12 +1,12 @@
 const {
   registration
 } = require('../../model')
-const { sendMail } = require('../../helpers/sendMail')
-  
+// const { sendMail } = require('../../helpers/sendMail')
+
 const registrationController = async (req, res) => {
   const { email, password } = req.body
   const newUser = await registration(email, password)
-  await sendMail(email, newUser.verificationToken)
+  // await sendMail(email, newUser.verificationToken)
 
   res.status(201).json({ newUser })
 }

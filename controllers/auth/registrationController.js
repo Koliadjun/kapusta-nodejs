5e88c89224c0924e8c50d499f240e2387516a2e4
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-23 20:23:30 +0200

    fix

--- controllers/auth/registrationController.js
+++ controllers/auth/registrationController.js
@@ -6,7 +6,7 @@ const {
 const registrationController = async (req, res) => {
   const { email, password } = req.body
   const newUser = await registration(email, password)
-  // await sendMail(email, newUser.verificationToken)
+  await sendMail(email, newUser.verificationToken)
 
   res.status(201).json({ newUser })
 }

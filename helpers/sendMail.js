51423e9a262057b8d85dcc7c69dac1969fa7bf1e
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-23 15:21:24 +0200

    fix link

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 sendGrid.setApiKey(process.env.GRID_SEND_APIKEY)
 
 const sendMail = async (email, verificationToken) => {
-  const mailMarkup = `<p>Hello there! Please verify your email by clicking that button!</p> <button type="button"><a href="http://localhost:${process.env.PORT}/api/auth/verify/${verificationToken}">BUTTON</a></button>`
+  const mailMarkup = `<p>Hello there! Please verify your email by clicking that button!</p> <button type="button"><a href="https://kapusta-api-iteam.herokuapp.com/api/auth/verify/${verificationToken}">BUTTON</a></button>`
   const msg = {
     to: email,
     from: 'Koliadjun@gmail.com',

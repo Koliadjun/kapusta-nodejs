174f9a0241c19ae0ea7f558133bdcdfa5d68d64a
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-26 18:22:08 +0200

    fix current

--- model/auth/current.js
+++ model/auth/current.js
@@ -4,9 +4,8 @@ const jsonwebtoken = require('jsonwebtoken')
 
 const current = async (token) => {
     const { _id } = await jsonwebtoken.verify(token, process.env.SECRET_KEY)
-    console.log(_id)
     const user = await UserModel.findById(_id)
-    if (!user) {
+    if (!user || user.token !== token) {
         throw new Unauthorized('No such user!')
     }
     // const userData = {

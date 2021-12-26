775770e61ae7eaa2cd415b4d2d982b11c339399c
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-26 16:00:57 +0200

    fix validation

--- middlewares/schemaJoi.js
+++ middlewares/schemaJoi.js
@@ -1,7 +1,7 @@
 const Joi = require('joi')
 
 const schema = Joi.object({
-  sum: Joi.number().min(1).max(30),
+  sum: Joi.number(),
   date: Joi.string(),
   year: Joi.number(),
   month: Joi.number(),
